8eba2ea84e6394c77c8f8d4f0b70a47aa5e8b459
JungSuIn <suin6327@naver.com>
2025-05-29 02:36:21 +0900

    Initial Commit

---
+++ MeetingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeetingApp
+//
+//  Created by 정수인 on 5/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeetingApp/MeetingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingAppApp.swift
+//  MeetingApp
+//
+//  Created by 정수인 on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MeetingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
